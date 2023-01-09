a15ee2a4a98dc59989089773f0f201d540f1fa0c
reecehuff <reecedhuff@gmail.com>
2023-01-08 20:48:01 -0800

    Added evaluation utils

--- core/utils.py
+++ core/utils.py
@@ -56,9 +56,16 @@ def load_model(args, model_type, train=False):
         model_name = args.RigidNet_name
 
     # Load the model
-    model = torch.load(os.path.join(args.model_dir, model_name + '.pt'))
+    if args.resume:
+        print('Loading model from: {}'.format(args.resume))
+        model.load_state_dict(torch.load(args.resume, map_location=torch.device(args.device)))
+    else:
+        print('Loading model from: {}'.format(os.path.join(args.model_dir, model_name + '.pt')))
+        model.load_state_dict(torch.load(os.path.join(args.model_dir, model_name + '.pt'), map_location=torch.device(args.device)))
+
     # Send the model to the device
     model.to(args.device)
+
      # Set the model to training or evaluation
     if train:
         model.train()
@@ -133,6 +140,53 @@ def get_paths(path):
 
     return paths
 
+# Define a function for getting the paths for the images or strains for evalation
+def get_eval_paths(path, sampling_rate=1):
+
+    # Get all of the paths 
+    paths = sorted(glob.glob(os.path.join(path, '*/*/*'), recursive=True))
+
+    # Separate the paths for the images and strains
+    image1paths = []
+    image2paths = []
+    strain_xx_paths = []
+    strain_yy_paths = []
+    strain_xy_paths = []
+    for path in paths:
+        if 'im1' in path:
+            image1paths.append(path)
+        elif 'im2' in path:
+            image2paths.append(path)
+        elif 'xx' in path:
+            strain_xx_paths.append(path)
+        elif 'yy' in path:
+            strain_yy_paths.append(path)
+        elif 'xy' in path:
+            strain_xy_paths.append(path)
+
+    # Sort the paths
+    image1paths = sorted(image1paths)
+    image2paths = sorted(image2paths)
+    strain_xx_paths = sorted(strain_xx_paths)
+    strain_yy_paths = sorted(strain_yy_paths)
+    strain_xy_paths = sorted(strain_xy_paths)
+
+    # Sample the paths
+    image1paths = image1paths[::sampling_rate]
+    image2paths = image2paths[::sampling_rate]
+    strain_xx_paths = strain_xx_paths[::sampling_rate]
+    strain_yy_paths = strain_yy_paths[::sampling_rate]
+    strain_xy_paths = strain_xy_paths[::sampling_rate]
+
+    # Create a dictionary of the paths
+    paths = {'image1': image1paths, 
+             'image2': image2paths, 
+             'strain_xx': strain_xx_paths, 
+             'strain_yy': strain_yy_paths, 
+             'strain_xy': strain_xy_paths}
+
+    return paths
+
 # Define a function for getting the data loader for training or validation
 def get_data_loader(args, model_type, train=True):
 
@@ -156,6 +210,27 @@ def get_data_loader(args, model_type, train=True):
 
     # Create the data loader
     data_loader = DataLoader(data_set, batch_size=args.batch_size, shuffle=train)
+
+    return data_loader
+
+# Define a function for getting the data loader for evaluation
+def get_eval_data_loader(args):
+
+    # Gather the paths for the images and strains
+    paths = get_eval_paths(args.val_data_dir, sampling_rate=args.sampling_rate)
+
+    # Loop through the paths and print them 
+    for key in paths.keys():
+        print(key)
+        for path in paths[key]:
+            print(path)
+
+    
+    # Create DataSet object
+    data_set = Dataset_4_Regression(paths)
+
+    # Create the data loader
+    data_loader = DataLoader(data_set, batch_size=args.batch_size, shuffle=False)
 
     return data_loader
 
@@ -187,23 +262,29 @@ def save_model(model, args, model_type):
         model_name = args.RigidNet_name
 
     # Save the model both to the save directory and to the logging directory
-    torch.save(model, os.path.join(args.model_dir, model_name + '.pt'))
-    torch.save(model, os.path.join(args.log_dir, model_name + '.pt'))
+    torch.save(model.state_dict(), os.path.join(args.model_dir, model_name + '.pt'))
+    torch.save(model.state_dict(), os.path.join(args.log_dir, model_name + '.pt'))
 
 # Define a function for obtaining the transforms
-def get_transform(transform_type):
+def get_transform(args, transform_type):
 
     # Define the transforms
     if transform_type == 'train':
         transform = transforms.Compose([
+                                        transforms.ToPILImage(),
+                                        transforms.Resize((args.input_height, args.input_width)),
                                         transforms.ToTensor()
                                         ])
     elif transform_type == 'valid':
         transform = transforms.Compose([
+                                        transforms.ToPILImage(),
+                                        transforms.Resize((args.input_height, args.input_width)),
                                         transforms.ToTensor()
                                         ])
     elif transform_type == 'test':
         transform = transforms.Compose([
+                                        transforms.ToPILImage(),
+                                        transforms.Resize((args.input_height, args.input_width)),
                                         transforms.ToTensor()
                                         ])
     else:
@@ -213,6 +294,11 @@ def get_transform(transform_type):
 
 # Define a function for getting the deformation class based sum of the strains in the xx direction
 def get_deformation_class(strains):
+
+    # If the input is a tensor, convert it to a numpy array
+    if torch.is_tensor(strains):
+        strains = strains.numpy()
+
     # Isolate the strain in the xx direction
     if len(strains.shape) == 3:
         strain_xx = strains[0, :, :]
@@ -233,6 +319,9 @@ def get_deformation_class(strains):
 
 # Define a function for getting the deformation type 
 def get_deformation_type(class_pred):
+    
+    # Convert the class prediction to the deformation type
+    class_pred = softmax_to_class(class_pred)
 
     # Get the deformation type
     if class_pred == 1:
@@ -274,6 +363,7 @@ def softmax_to_class(softmax_output):
 # Define a function for adjusting the data directories depending on the model type
 def get_data_dirs(args):
 
+    # Adjust the data directories depending on the model type
     if args.model_type == 'DeformationClassifier':
         pass 
     elif args.model_type == 'TensionNet':
@@ -293,6 +383,24 @@ def get_data_dirs(args):
     assert os.path.exists(args.val_data_dir), 'Validation data directory does not exist'
 
     return args
+
+# Define a function for gathering the data directories for evaluation
+def get_eval_data_dirs(args):
+
+    # If dealing with sequential data
+    if args.sequential:
+        
+        # Assert that the data directories exist
+        assert os.path.exists(args.val_data_dir), 'Validation data directory does not exist'
+
+    # If dealing with a standard validation dataset 
+    else:
+
+        # Assert that the data directories exist
+        assert os.path.exists(args.val_data_dir), 'Validation data directory does not exist'
+
+    return args
+
 
 # Define a function setting the random seeds
 def set_random_seeds(seed):
