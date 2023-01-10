b89f5c8487ae5471bcd5d13293c844a22b1e82d4
reecehuff <reecedhuff@gmail.com>
2023-01-09 22:47:10 -0800

    For running on all synthetic test cases

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python eval.py --val_data_dir datasets/04DEF/ --sequential
+for DEF in "04DEF" "07DEF" "10DEF" "13DEF" "16DEF"; 
+do 
+    echo $DEF
+    python eval.py --val_data_dir datasets/SyntheticTestCases/$DEF/ --sequential --custom_sampling --log_dir results/syntheticTestCase/$DEF/ --visualize --save_strains
+   
+done 
