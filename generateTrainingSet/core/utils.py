0275f3745755b9f21c744327ff4e1ad8e0361859
reecehuff <reecedhuff@gmail.com>
2023-01-09 22:50:38 -0800

    Cleaned up utils

--- generateTrainingSet/core/utils.py
+++ generateTrainingSet/core/utils.py
@@ -8,114 +8,64 @@ import matplotlib.pyplot as plt
 
 #%% Functions
 
-# Define a function that gathers the paths of the images or the masks that will be used to generate the training set
-def gatherPaths(path):
-    """
-    gatherPaths gathers the paths of the images or the masks that will be used to generate the training set.
+# Define a class for getting things
+class get(object):
+
+    @staticmethod
+    def paths(path):
+        """
+        getPaths gets the paths to the images or masks.
+        """
+
+        # Gather the paths to the images or masks
+        output = glob.glob(path + '/*')
+
+        # Sort the paths
+        output.sort()
+
+        return output        
+
+# Define a class for checking things
+class check(object):
     
-    The inputs are:
-        path - The path to the directory that contains the images or masks (string).
+    @staticmethod
+    def image_and_mask_file_names(path2images, path2masks):
+        """
+        checkImageAndMaskFileNames checks that the names of the images and masks are the same.
+        """
 
-    The outputs are:
-        path2 - The paths to the images or the masks (list of strings).
-    """
-    # Gather the paths to the images or masks
-    path2 = glob.glob(path + '/*')
+        # Check that the names of the images and masks are the same
+        for i in range(len(path2images)):
+            assert path2images[i].split('/')[-1].split('.')[0] == path2masks[i].split('/')[-1].split('.')[0], 'The names of the images and masks must be the same.'
 
-    # Sort the paths
-    path2.sort()
+class load(object):
     
-    return path2
-
-# Define a function that checks that the names of the images and masks are the same
-def checkImageMaskFilesNames(path2images, path2masks):
-    """
-    checkImageMaskFilesNames checks that the names of the images and masks are the same.
+    @staticmethod
+    def image_and_mask(path2image, path2mask):
+        """
+        loadImageAndMask loads the image and mask.
+        
+        The inputs are:
+            path2image - The path to the image (string).
+            path2mask - The path to the mask (string).
     
-    The inputs are:
-        path2images - The paths to the images (list of strings).
-        path2masks - The paths to the masks (list of strings).
-
-    """
-    # Check that the names of the images and masks are the same
-    for i in range(len(path2images)):
-        assert path2images[i].split('/')[-1].split('.')[0] == path2masks[i].split('/')[-1].split('.')[0], 'The names of the images and masks must be the same.' 
-
-# Define a function that loads the image and mask
-def loadImageAndMask(path2image, path2mask):
-    """
-    loadImageAndMask loads the image and mask.
+        The outputs are:
+            img - The image (numpy array).
+            mask - The mask (numpy array).
     
-    The inputs are:
-        path2image - The path to the image (string).
-        path2mask - The path to the mask (string).
-
-    The outputs are:
-        img - The image (numpy array).
-        mask - The mask (numpy array).
-
-    """
-    # Load the image and mask
-    img = cv2.imread(path2image, cv2.IMREAD_GRAYSCALE)
-    mask = cv2.imread(path2mask, cv2.IMREAD_GRAYSCALE)
-
-    # Convert the image and mask to float32
-    img = img.astype(np.float32)
-    mask = mask.astype(np.float32)
-
-    # Convert the mask to a binary mask
-    mask[mask > 0] = 1
-
-    return img, mask
-
-# Define a function that warps the image using the displacement field
-def imwarp(img, displacement_field):
-    """
-    imwarp warps the image using the displacement field.
+        """
+        # Load the image and mask
+        img = cv2.imread(path2image, cv2.IMREAD_GRAYSCALE)
+        mask = cv2.imread(path2mask, cv2.IMREAD_GRAYSCALE)
     
-    The inputs are:
-        img - The image (numpy array).
-        displacement_field - The displacement field (numpy array).
-
-    The outputs are:
-        img1 - The first image (numpy array).
-        img2 - The second image (numpy array).
-
-    """
-
-    # The first image is the original image
-    img1 = img
-    # The second image is the warped image
-    h, w = displacement_field.shape[:2]
-    displacement_field_4_warping = np.zeros((h, w, 2), dtype=np.float32)
-    displacement_field_4_warping[:,:,0] = -displacement_field[:,:,0]
-    displacement_field_4_warping[:,:,1] = -displacement_field[:,:,1]
-    displacement_field_4_warping[:,:,0] += np.arange(w)
-    displacement_field_4_warping[:,:,1] += np.arange(h)[:,np.newaxis]
-    img2 = cv2.remap(img, displacement_field_4_warping, None, cv2.INTER_LINEAR)
-    # img2 = cv2.remap(img, displacement_field, None, cv2.INTER_LINEAR)
-
-    return img1, img2
-
-# Define a function that adds noise to the image
-def addNoise(img, noise):
-    """
-    addNoise adds noise to the image.
+        # Convert the image and mask to float32
+        img = img.astype(np.float32)
+        mask = mask.astype(np.float32)
     
-    The inputs are:
-        img - The image (numpy array).
-
-    The outputs are:
-        img - The image with noise (numpy array).
-
-    """
-    # Add noise to the image
-    img = img + np.random.normal(0, noise, img.shape)
-
-    # Clip the image
-    img = np.clip(img, 0, 255)
-
-    return img
+        # Convert the mask to a binary mask
+        mask[mask > 0] = 1
+    
+        return img, mask
 
 # Define a function that saves the images, displacement field, and strain fields
 def saveData(img1, img2, displacement_field, strain, path2save, COUNT):
@@ -278,3 +228,9 @@ def visualizeData(img1, img2, displacement_field, strain, path2save, COUNT):
 
     # Close the figure
     plt.close()
+
+# Define a function setting the random seeds
+def set_random_seeds(seed):
+    # Set the random seed
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
