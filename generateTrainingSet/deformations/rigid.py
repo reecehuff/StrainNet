45410743b7922a99b20ad36890ab81fe972ec6d3
reecehuff <reecedhuff@gmail.com>
2023-01-10 21:16:15 -0800

    Added linear and quadratic fields
    Note: image 1 and image 2 are the same warped image and therefore the strain is still zero

--- generateTrainingSet/deformations/rigid.py
+++ generateTrainingSet/deformations/rigid.py
@@ -35,18 +35,61 @@ class one: # Rigid 1
 
         return strain_xx, strain_yy, strain_xy
 
-class two: # Rigid 2
+class two: # Rigid 2 (note: img1 and img2 will both be the image warped by this displacement field)
 
+    # Define a function that calculates the displacement in the x and y directions
     @staticmethod
     def calculateDisplacement(self):
 
-        # Calculate the displacement in the x and y directions
-        displacement_x = self.u_x
-        displacement_y = self.u_y
+        # Unpack the inputs
+        epsilon_xx_center = self.epsilon_xx_center
+        nu = self.nu
+        x_c = self.x_c
+        y_c = self.y_c
+        a = self.a
+        x = self.x
+        y = self.y
 
-        # Add rotation to the displacement about the centroid of the mask
-        displacement_x = displacement_x * np.cos(np.deg2rad(self.rotation)) - displacement_y * np.sin(np.deg2rad(self.rotation))
-        displacement_y = displacement_x * np.sin(np.deg2rad(self.rotation)) + displacement_y * np.cos(np.deg2rad(self.rotation))
+        # Calculate the displacement in the x and y directions
+        displacement_x = a*(x-x_c)*(y-y_c)**2 + epsilon_xx_center*(x-x_c)
+        displacement_y = -nu * ( (a/3)*((y-y_c)**3) + epsilon_xx_center*(y-y_c) )
+
+        return displacement_x, displacement_y
+
+    # Define a function that calculates the strain in the xx, yy and xy direction
+    @staticmethod
+    def calculateStrain(self):
+
+        # Calculate the strain in the xx, yy and xy directions
+        strain_xx = 0
+        strain_yy = 0
+        strain_xy = 0
+
+        # Convert strain to percent
+        strain_xx = strain_xx * 100
+        strain_yy = strain_yy * 100
+        strain_xy = strain_xy * 100
+
+        return strain_xx, strain_yy, strain_xy
+
+class three: # Rigid 3 (note: img1 and img2 will both be the image warped by this displacement field)
+
+    # Define a function that calculates the displacement in the x and y directions
+    @staticmethod
+    def calculateDisplacement(self):
+
+        # Unpack the inputs
+        x = self.x
+        y = self.y
+        nu = self.nu
+        y_c = self.y_c
+        epsilon_xx_proximal = self.epsilon_xx_proximal
+        epsilon_xx_distal = self.epsilon_xx_distal
+        w = self.W
+
+        # Calculate the displacement in the x and y directions
+        displacement_x = epsilon_xx_distal * x + (epsilon_xx_proximal - epsilon_xx_distal) * (x ** 2 / (2 * w))
+        displacement_y = - nu * (epsilon_xx_distal * (y - y_c) + (epsilon_xx_proximal - epsilon_xx_distal) * (x * (y - y_c) / w))
 
         return displacement_x, displacement_y
 
