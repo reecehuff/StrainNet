be3151cff70e15cb352796d95a67e5502b61b933
reecehuff <reecedhuff@gmail.com>
2023-01-10 21:19:28 -0800

    Fixed train_all flag

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-python train.py --train_all
+# # For training all of the models, run the following command:
+# python train.py --train_all \
+#                 --epochs 2 \
+#                 --train_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/training/ \
+#                 --val_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/validation/ \
+#                 --experiment_name train_all
+
+# # For training a single model, run the following command:
+python train.py --model_type DeformationClassifier \
+                --epochs 2 \
+                --train_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/training/ \
+                --val_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/validation/ \
+                --experiment_name train_DC
+
+# For training a subset of the four models, run the following command:
+python train.py --train_all \
+                --model_types TensionNet CompressionNet RigidNet \
+                --epochs 2 \
+                --train_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/training/ \
+                --val_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/validation/ \
+                --experiment_name train_TN_CN_RN
