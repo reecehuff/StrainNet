0eab369463479b4bca42bc37d3523af09fb4d013
reecehuff <reecedhuff@gmail.com>
2023-01-10 21:19:14 -0800

    Added example of resuming training on an old model

---
+++ scripts/resume.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# For resuming training of a model, run the following command:
+python train.py --resume models/20230110-201258/ \
+                --model_type DeformationClassifier \
+                --epochs 2 \
+                --train_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/training/ \
+                --val_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/validation/ 
