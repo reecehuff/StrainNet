b07d845600fcc2b141085d80bf5b302e606316f2
reecehuff <reecedhuff@gmail.com>
2023-01-11 17:19:56 -0800

    New training set

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,22 +2,21 @@
 
 # # For training all of the models, run the following command:
 # python train.py --train_all \
-#                 --epochs 2 \
-#                 --train_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/training/ \
-#                 --val_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/validation/ \
+#                 --train_data_dir datasets/train_set_N_tension_1250_N_compression_1250_N_rigid_1250/training/ \
+#                 --val_data_dir datasets/train_set_N_tension_1250_N_compression_1250_N_rigid_1250/validation/ \
 #                 --experiment_name train_all
 
-# # For training a single model, run the following command:
+# For training a single model, run the following command:
 python train.py --model_type DeformationClassifier \
-                --epochs 2 \
-                --train_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/training/ \
-                --val_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/validation/ \
+                --batch_size 100 \
+                --train_data_dir datasets/train_set_N_tension_1250_N_compression_1250_N_rigid_1250/training/ \
+                --val_data_dir datasets/train_set_N_tension_1250_N_compression_1250_N_rigid_1250/validation/ \
                 --experiment_name train_DC
 
 # For training a subset of the four models, run the following command:
 python train.py --train_all \
+                --batch_size 10 \
                 --model_types TensionNet CompressionNet RigidNet \
-                --epochs 2 \
-                --train_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/training/ \
-                --val_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/validation/ \
+                --train_data_dir datasets/train_set_N_tension_1250_N_compression_1250_N_rigid_1250/training/ \
+                --val_data_dir datasets/train_set_N_tension_1250_N_compression_1250_N_rigid_1250/validation/ \
                 --experiment_name train_TN_CN_RN
