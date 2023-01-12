5883bcb71b4293c45898e8420db418cb071534f9
reecehuff <reecedhuff@gmail.com>
2023-01-11 23:20:27 -0800

    Added strain image overlay function

--- core/visualize.py
+++ core/visualize.py
@@ -26,6 +26,14 @@ def visualize_and_save_errors(args, imgs, strains, running_strains, error_image,
     # Save error image
     save_error_image(error_image, save_path, args)
 
+def visualize_experiment(args, imgs, strains, data_loader):
+
+    # Get the save path for visualizing the images and strains
+    save_path = get_visualization_save_path(args, data_loader, init=False)
+    
+    # Visualize the images and strains
+    visualize_exp(imgs, strains, save_path, args)
+
 def init_visualization(args, imgs, strains, data_loader):
 
     # For the very first image, we want to visualize the images and strains
@@ -40,6 +48,7 @@ def init_visualization(args, imgs, strains, data_loader):
 
     # Get the save path for visualizing the images and strains
     save_path = get_visualization_save_path(args, data_loader, init=True)
+
     # Visualize the images and strains
     visualize_eval(imgs, strains, running_strains, error_image, save_path, args)
 
@@ -47,6 +56,23 @@ def init_visualization(args, imgs, strains, data_loader):
     save_path = get_error_image_save_path(args, data_loader, init=True)
     # Save error image
     save_error_image(error_image, save_path, args)
+
+def exp_init_visualization(args, imgs, data_loader):
+
+    # For the very first image, we want to visualize the images and strains
+    # The image pair will just be the same image twice
+    # The strain will be zero
+    # The running strain will be zero
+    # The error image will be zero
+    imgs[:, 1, :, :] = imgs[:, 0, :, :]
+    B, C, H, W = imgs.shape
+    strains = torch.zeros((B, C+1, H, W))
+
+    # Get the save path for visualizing the images and strains
+    save_path = get_visualization_save_path(args, data_loader, init=True)
+    
+    # Visualize the images and strains
+    visualize_exp(imgs, strains, save_path, args)
 
 def get_visualization_save_path(args, data_loader, init=False):
 
@@ -72,7 +98,7 @@ def get_error_image_save_path(args, data_loader, init=False):
 
     return save_path 
 
-# Define a function that visualizes the images, displacement field, and strain fields
+# Define a function that visualizes the images and strains
 def visualize_eval(imgs, strains, running_strains, error_image, savePath, args):
     """
     visualizeData visualizes the images, the predicted strains, and the true strains.
@@ -190,6 +216,91 @@ def visualize_eval(imgs, strains, running_strains, error_image, savePath, args):
     # Close the figure
     plt.close()
 
+# Define a function that visualizes the images, displacement field, and strain fields
+def visualize_exp(imgs, strains, savePath, args):
+    """
+    visualizeData visualizes the images and the predicted strains.
+    
+    The inputs are:
+        imgs - The images (tensor).
+        strains - The predicted strains (tensor).
+        savePath - The path to save the visualization.
+        args - The arguments (dictionary).
+    """
+
+    # Create the directory to save the images and strain fields visualization
+    # The visualization path is the save path with the last directory removed
+    visualizePath = os.path.dirname(savePath)
+    if not os.path.exists(visualizePath):
+        os.makedirs(visualizePath)
+
+    # Begin by splitting the images and strain fields
+    img1 = imgs[0, 0, :, :].cpu().numpy()
+    img2 = imgs[0, 1, :, :].cpu().numpy()
+    strain = strains[0, :, :, :].cpu().numpy()
+    pred_strain_xx = strain[0, :, :]
+    pred_strain_yy = strain[1, :, :]
+    pred_strain_xy = strain[2, :, :]
+
+    # Squeeze all of the tensors
+    img1 = np.squeeze(img1)
+    img2 = np.squeeze(img2)
+    pred_strain_xx = np.squeeze(pred_strain_xx)
+    pred_strain_yy = np.squeeze(pred_strain_yy)
+    pred_strain_xy = np.squeeze(pred_strain_xy)
+
+    # Create a massive matplotlib figure
+    plt.figure(figsize=(20, 10))
+
+    # Add the second image with the predicted strain xx overlaid
+    plt.subplot(1, 3, 1)
+    colorbarplot_xx = plt.imshow(pred_strain_xx,alpha=1, cmap='viridis', vmin=color_bar_range_xx[0], vmax=color_bar_range_xx[1])
+    plt.imshow(img2, alpha=0.7, cmap=plt.cm.gray)
+    plt.title('Predicted strain ($\epsilon_{xx}^{pred}$)', fontsize=20)
+    # Add the colorbar
+    cbar = plt.colorbar(colorbarplot_xx)
+    cbar.set_label(label="strain [%]", size=16)
+    # Adjust the colorbar range
+    cbar.set_ticks(np.ceil(np.linspace(color_bar_range_xx[0], color_bar_range_xx[1], 5)))
+    # Make sure the colorbar ticks do not have decimal points
+    cbar.ax.set_yticklabels(cbar.ax.get_yticklabels(), fontsize=16)
+
+    # Add the second image with the predicted strain yy overlaid
+    plt.subplot(1, 3, 2)
+    colorbarplot_yy = plt.imshow(pred_strain_yy,alpha=1, cmap='viridis', vmin=color_bar_range_yy[0], vmax=color_bar_range_yy[1])
+    plt.imshow(img2, alpha=0.7, cmap=plt.cm.gray)
+    plt.title('Predicted strain ($\epsilon_{yy}^{pred}$)', fontsize=20)
+    # Add the colorbar
+    cbar = plt.colorbar(colorbarplot_yy)
+    cbar.set_label(label="strain [%]", size=16)
+    # Adjust the colorbar range
+    cbar.set_ticks(np.ceil(np.linspace(color_bar_range_yy[0], color_bar_range_yy[1], 5)))
+    # Make sure the colorbar ticks do not have decimal points
+    cbar.ax.set_yticklabels(cbar.ax.get_yticklabels(), fontsize=16)
+
+    # Add the second image with the predicted strain xy overlaid
+    plt.subplot(1, 3, 3)
+    colorbarplot_xy = plt.imshow(pred_strain_xy,alpha=1, cmap='viridis', vmin=color_bar_range_xy[0], vmax=color_bar_range_xy[1])
+    plt.imshow(img2, alpha=0.7, cmap=plt.cm.gray)
+    plt.title('Predicted strain ($\epsilon_{xy}^{pred}$)', fontsize=20)
+    # Add the colorbar
+    cbar = plt.colorbar(colorbarplot_xy)
+    cbar.set_label(label="strain [%]", size=16)
+    # Adjust the colorbar range
+    cbar.set_ticks(np.ceil(np.linspace(color_bar_range_xy[0], color_bar_range_xy[1], 5)))
+    # Make sure the colorbar ticks do not have decimal points
+    cbar.ax.set_yticklabels(cbar.ax.get_yticklabels(), fontsize=16)
+
+    # Add a title to the figure
+    super_title = 'Strains Predicted by StrainNet: ' + os.path.basename(savePath)
+    plt.suptitle(super_title, fontsize=30)
+
+    # Save the figure
+    plt.savefig(savePath, bbox_inches='tight')
+
+    # Close the figure
+    plt.close()
+
 # Define a function for creating a Bayesian plot
 def create_bayesian_plot(xaxis,line,upper,lower,label,fill_color):
     # Accuracy plots
@@ -239,6 +350,42 @@ def visualize_errors(performance, args):
 
     # Save the figure
     savePath = os.path.join(args.log_dir, "median_errors.png")
+    plt.savefig(savePath, bbox_inches='tight')
+
+# Define a function for visualizing the strains using Bayesian plots
+def visualize_strains(predictions, args):
+    '''
+    Visualizes the strains using Bayesian plots
+    '''
+
+    # Begin by unraveling the predictions dictionary
+    indices = predictions["Frame Number"]
+    bulk_median_strain_xx = predictions["bulk_median_strain_xx"]
+    bulk_75th_strain_xx = predictions["bulk_75th_strain_xx"]
+    bulk_25th_strain_xx = predictions["bulk_25th_strain_xx"]
+
+    # Create a figure
+    plt.figure(figsize=(10, 10))
+
+    # Define the inputs to the Bayesian plot
+    xaxis = indices
+    line = bulk_median_strain_xx
+    upper = bulk_75th_strain_xx
+    lower = bulk_25th_strain_xx
+    label = "Median strains (25th - 75th percentile)"
+    fill_color = "blue"
+    create_bayesian_plot(xaxis,line,upper,lower,label,fill_color)
+
+    # Make the plot better looking
+    plt.xlabel('Frame number', fontsize=20)
+    plt.ylabel('Strain XX [%]', fontsize=20)
+    plt.title('Strains over time', fontsize=30)
+    
+    # Increase the font size of the tick labels
+    plt.tick_params(axis='both', which='major', labelsize=15)
+
+    # Save the figure
+    savePath = os.path.join(args.log_dir, "median_strains.png")
     plt.savefig(savePath, bbox_inches='tight')
 
 # Define a function that saves error images as npy arrays for later use
