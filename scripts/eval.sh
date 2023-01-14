6455aa1d16aae142066147b3567548abd4e1ffbc
reecehuff <reecedhuff@gmail.com>
2023-01-13 23:25:41 -0800

    Finalized eval script

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -2,10 +2,10 @@
 for DEF in "04DEF" "07DEF" "10DEF" "13DEF" "16DEF"; 
 do 
     echo $DEF
-    python eval.py  --val_data_dir datasets/SyntheticTestCases/$DEF/ \
+    python eval.py  --val_data_dir datasets/synthetic/$DEF/ \
                     --sequential \
                     --custom_sampling \
-                    --log_dir results/syntheticTestCase/$DEF/ \
+                    --log_dir results/synthetic/$DEF/ \
                     --visualize \
                     --save_strains
    
