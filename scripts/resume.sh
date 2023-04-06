4908a2019b0eec0b848af200104cf7cb6029760e
reecehuff <reecedhuff@gmail.com>
2023-04-06 16:51:59 -0700

    Cleaned up

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # For resuming training of a model, run the following command:
-python train.py --resume models/20230110-201258/ \
+python train.py --resume models/train_DC/ \
                 --model_type DeformationClassifier \
                 --epochs 2 \
                 --train_data_dir datasets/train_set_N_tension_5_N_compression_5_N_rigid_5/training/ \
